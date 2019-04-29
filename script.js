8e975e69a3b9a2afc7a4898b442c617b98765866
luvensaitory <luvensaitory@gmail.com>
2019-04-29 10:18:08 +0800

    part1 layout done.

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
+
+function onClick(event) {
+    const div = event.currentTarget;
+    div.style.backgroundColor = "#cfe3ff";
+    console.log(div);
+    const ckbox = document.querySelector('.checkbox');
+    ckbox.src = 'images/checked.png';
+    div.removeEventListener('click', onClick);
+}
+
+const images = document.querySelectorAll('.choice-grid div');
+for (const image of images) {
+    image.addEventListener('click', onClick);
+}
